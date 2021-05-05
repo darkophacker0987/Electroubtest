2153fdc5fb9d195f176f5aaa2b297b8c201248e0
Gaurav Verma <gauravv2504@gmail.com>
2021-05-05 19:30:46 +0530

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -3,3 +3,17 @@ from userbot.utils import *
 from userbot.Config import Config
 from userbot.helpers.functions import *
 from userbot.cmdhelp import CmdHelp
+
+
+
+# =================== CONSTANT ===================
+
+USERID = bot.uid if Config.OWNER_ID == 0 else Config.OWNER_ID
+ALIVE_NAME = Config.ALIVE_NAME
+AUTONAME = Config.AUTONAME
+DEFAULT_BIO = Config.DEFAULT_BIO
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
+BOT_USERNAME = Config.TG_BOT_USERNAME
+# mention user
+mention = f"[{DEFAULTUSER}](tg://user?id={USERID})"
+hmention = f"<a href = tg://user?id={USERID}>{DEFAULTUSER}</a>"
