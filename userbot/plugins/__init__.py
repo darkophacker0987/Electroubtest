d648681b9f727a61ab9aa788c54b2abdf700ab81
Gaurav Verma <gauravv2504@gmail.com>
2021-05-05 19:39:36 +0530

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -1,19 +1,19 @@
 from userbot import *
-from userbot.utils import *
 from userbot.Config import Config
 from userbot.helpers.functions import *
 from userbot.cmdhelp import CmdHelp
+from AuraXBot.utils import *
+from telethon import events, version
+from telethon.events import NewMessage
+from telethon.tl.custom import Dialog
+from telethon.tl.types import Channel, Chat, User
 
 
 
 # =================== CONSTANT ===================
 
 USERID = bot.uid if Config.OWNER_ID == 0 else Config.OWNER_ID
-ALIVE_NAME = Config.ALIVE_NAME
-AUTONAME = Config.AUTONAME
-DEFAULT_BIO = Config.DEFAULT_BIO
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
-BOT_USERNAME = Config.TG_BOT_USERNAME
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "AuraX User"
 # mention user
 mention = f"[{DEFAULTUSER}](tg://user?id={USERID})"
 hmention = f"<a href = tg://user?id={USERID}>{DEFAULTUSER}</a>"
