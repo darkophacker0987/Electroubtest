166ecfa265a30ed1b9744867d6fe527772c3adef
Gaurav Verma <gauravv2504@gmail.com>
2021-05-13 01:57:44 +0530

    Update purges.py

--- AuraXUserbot/plugins/purges.py
+++ AuraXUserbot/plugins/purges.py
@@ -3,7 +3,7 @@ import asyncio
 import telethon.utils
 from telethon import events
 from AuraXBot.utils import admin_cmd, sudo_cmd, edit_or_reply
-from userbot.cmdhelp import CmdHelp
+from AuraXUserbot.cmdhelp import CmdHelp
 from uniborg.util import re
 
 
