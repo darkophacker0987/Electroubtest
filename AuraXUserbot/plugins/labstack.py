85c9ca7e8cf2961572f156daaf9494767ade166e
Gaurav Verma <gauravv2504@gmail.com>
2021-05-13 01:55:39 +0530

    Update labstack.py

--- AuraXUserbot/plugins/labstack.py
+++ AuraXUserbot/plugins/labstack.py
@@ -4,7 +4,7 @@ import subprocess
 
 import requests
 from AuraXBot.utils import admin_cmd, sudo_cmd, edit_or_reply
-from userbot.cmdhelp import CmdHelp
+from AuraXUserbot.cmdhelp import CmdHelp
 
 
 @bot.on(admin_cmd(pattern="labstack ?(.*)", outgoing=True))
