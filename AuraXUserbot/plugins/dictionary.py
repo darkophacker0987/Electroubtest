640d9ccf4e586568a46187489a26d72131573d03
Gaurav Verma <gauravv2504@gmail.com>
2021-05-13 01:49:09 +0530

    Update dictionary.py

--- AuraXUserbot/plugins/dictionary.py
+++ AuraXUserbot/plugins/dictionary.py
@@ -6,9 +6,9 @@ Syntax: .ud Query"""
 import asyncurban
 from PyDictionary import PyDictionary
 
-from userbot import CMD_HELP
+from AuraXUserbot import CMD_HELP
 from AuraXBot.utils import admin_cmd, edit_or_reply, sudo_cmd
-from userbot.cmdhelp import CmdHelp
+from AuraXUserbot.cmdhelp import CmdHelp
 
 @bot.on(admin_cmd(pattern="ud (.*)"))
 @bot.on(sudo_cmd(pattern="ud (.*)", allow_sudo=True))
