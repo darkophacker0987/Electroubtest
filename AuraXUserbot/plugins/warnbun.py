dd1778e578cb16aa9ea131cd15340193daa60149
Gaurav Verma <gauravv2504@gmail.com>
2021-05-13 01:50:48 +0530

    Update warnbun.py

--- AuraXUserbot/plugins/warnbun.py
+++ AuraXUserbot/plugins/warnbun.py
@@ -2,7 +2,7 @@
 from telethon.tl.types import ChannelParticipantsAdmins
 
 from AuraXBot.utils import admin_cmd, sudo_cmd, edit_or_reply
-from userbot.cmdhelp import CmdHelp
+from AuraXUserbot.cmdhelp import CmdHelp
 
 
 @bot.on(admin_cmd(pattern=r"warn1", outgoing=True))
