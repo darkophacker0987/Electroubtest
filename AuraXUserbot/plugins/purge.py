7208e7ca9226be40c7f430992dd80425ad6b87a7
Gaurav Verma <gauravv2504@gmail.com>
2021-05-13 01:57:41 +0530

    Update purge.py

--- AuraXUserbot/plugins/purge.py
+++ AuraXUserbot/plugins/purge.py
@@ -9,9 +9,9 @@ from asyncio import sleep
 
 from telethon.errors import rpcbaseerrors
 
-from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
+from AuraXUserbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
 from AuraXBot.utils import admin_cmd, errors_handler, sudo_cmd, edit_or_reply
-from userbot.cmdhelp import CmdHelp
+from AuraXUserbot.cmdhelp import CmdHelp
 
 
 
