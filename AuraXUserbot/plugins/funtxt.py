39f38f2e320ad674aeb91aa1b4d7427b29d5512c
Gaurav Verma <gauravv2504@gmail.com>
2021-05-13 01:54:35 +0530

    Update funtxt.py

--- AuraXUserbot/plugins/funtxt.py
+++ AuraXUserbot/plugins/funtxt.py
@@ -1,8 +1,8 @@
 import nekos
 
 from AuraXBot.utils import admin_cmd, edit_or_reply, sudo_cmd
-from userbot import CMD_HELP
-from userbot.cmdhelp import CmdHelp
+from AuraXUserbot import CMD_HELP
+from AuraXUserbot.cmdhelp import CmdHelp
 
 
 @bot.on(admin_cmd(pattern="ftext ?(.*)"))
