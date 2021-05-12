57c147e2cbec3e81626fc3c3f70115e88264d2c8
Gaurav Verma <gauravv2504@gmail.com>
2021-05-13 01:57:36 +0530

    Update pranks.py

--- AuraXUserbot/plugins/pranks.py
+++ AuraXUserbot/plugins/pranks.py
@@ -20,8 +20,8 @@ import os
 from telegraph import exceptions, upload_file
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 
-from userbot import CMD_HELP
-from userbot.helpers.functions import (
+from AuraXUserbot import CMD_HELP
+from AuraXUserbot.helpers.functions import (
     convert_toimage,
     deEmojify,
     phcomment,
@@ -30,7 +30,7 @@ from userbot.helpers.functions import (
     trash,
 )
 from AuraXBot.utils import admin_cmd, edit_or_reply, sudo_cmd
-from userbot.cmdhelp import CmdHelp
+from AuraXUserbot.cmdhelp import CmdHelp
 from . import *
 
 
