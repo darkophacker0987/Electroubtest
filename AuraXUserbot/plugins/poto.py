1482911db0fcfd4ae331ee3400f4ceb66d007aa2
Gaurav Verma <gauravv2504@gmail.com>
2021-05-13 01:57:30 +0530

    Update poto.py

--- AuraXUserbot/plugins/poto.py
+++ AuraXUserbot/plugins/poto.py
@@ -6,7 +6,7 @@
 import logging
 
 from AuraXBot.utils import admin_cmd, sudo_cmd, edit_or_reply
-from userbot.cmdhelp import CmdHelp
+from AuraXUserbot.cmdhelp import CmdHelp
 
 name = "Profile Photos"
 
