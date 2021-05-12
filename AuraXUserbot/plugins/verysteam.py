adfbd5fd26171a19c2ec83656d480ab39addfa00
Gaurav Verma <gauravv2504@gmail.com>
2021-05-13 01:50:53 +0530

    Update verysteam.py

--- AuraXUserbot/plugins/verysteam.py
+++ AuraXUserbot/plugins/verysteam.py
@@ -13,7 +13,7 @@ import aiohttp
 import magic
 import requests
 from AuraXBot.utils import admin_cmd, progress, sudo_cmd, edit_or_reply
-from userbot.cmdhelp import CmdHelp
+from AuraXUserbot.cmdhelp import CmdHelp
 
 
 @bot.on(admin_cmd(pattern="verystream ?(.*)", outgoing=True))
