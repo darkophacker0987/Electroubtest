684b9928ef66f13a4945e3fd4694c4fb6c716403
Gaurav Verma <gauravv2504@gmail.com>
2021-05-13 01:49:49 +0530

    Update utils.py

--- AuraXUserbot/utils.py
+++ AuraXUserbot/utils.py
@@ -20,13 +20,13 @@ from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
 
 from var import Var
 
-from userbot import CMD_LIST, LOAD_PLUG, LOGS, SUDO_LIST, bot
-from userbot.helpers.exceptions import CancelProcess
-from userbot.Config import Config
+from AuraXUserbot import CMD_LIST, LOAD_PLUG, LOGS, SUDO_LIST, bot
+from AuraXUserbot.helpers.exceptions import CancelProcess
+from AuraXUserbot.Config import Config
 
 ENV = bool(os.environ.get("ENV", False))
 if ENV:
-    from userbot.Config import Config
+    from AuraXUserbot.Config import Config
 else:
     if os.path.exists("config.py"):
         from config import Development as Config
