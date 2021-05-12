0ced1fd1b05f6ac4b855d9ef1b7890acfd2c5069
Gaurav Verma <gauravv2504@gmail.com>
2021-05-13 01:50:45 +0530

    Update watch.py

--- AuraXUserbot/plugins/watch.py
+++ AuraXUserbot/plugins/watch.py
@@ -8,7 +8,7 @@
 from justwatch import JustWatch
 
 from AuraXBot.utils import admin_cmd, sudo_cmd, edit_or_reply
-from userbot.cmdhelp import CmdHelp
+from AuraXUserbot.cmdhelp import CmdHelp
 
 
 def get_stream_data(query):
