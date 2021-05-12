cd58acc0993f5e44449cc00ec1261d1457289214
Gaurav Verma <gauravv2504@gmail.com>
2021-05-13 01:50:30 +0530

    Update welcome.py

--- AuraXUserbot/plugins/welcome.py
+++ AuraXUserbot/plugins/welcome.py
@@ -1,14 +1,14 @@
 from telethon import events
 from telethon.utils import pack_bot_file_id
 
-from userbot.plugins.sql_helper.welcome_sql import (
+from AuraXUserbot.plugins.sql_helper.welcome_sql import (
     add_welcome_setting,
     get_current_welcome_settings,
     rm_welcome_setting,
     update_previous_welcome,
 )
 from AuraXBot.utils import *
-from userbot.cmdhelp import CmdHelp
+from AuraXUserbot.cmdhelp import CmdHelp
 
 
 @bot.on(events.ChatAction())  # pylint:disable=E0602
