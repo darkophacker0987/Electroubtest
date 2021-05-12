2c42cc3c358d00684df2ef071e289cb355f6ebf8
Gaurav Verma <gauravv2504@gmail.com>
2021-05-13 01:56:03 +0530

    Update memes.py

--- AuraXUserbot/plugins/memes.py
+++ AuraXUserbot/plugins/memes.py
@@ -16,7 +16,7 @@ from cowpy import cow
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
 
-from userbot.cmdhelp import CmdHelp
+from AuraXUserbot.cmdhelp import CmdHelp
 from AuraXBot.utils import admin_cmd, sudo_cmd, edit_or_reply
 
 # ================= CONSTANT =================
