eea6de6a829b4a889c33138a5a39e4f833bed4e4
Gaurav Verma <gauravv2504@gmail.com>
2021-05-13 01:50:07 +0530

    Update wikimedia.py

--- AuraXUserbot/plugins/wikimedia.py
+++ AuraXUserbot/plugins/wikimedia.py
@@ -2,7 +2,7 @@
 Syntax: .wikimedia Query"""
 import requests
 from AuraXBot.utils import admin_cmd, sudo_cmd, edit_or_reply
-from userbot.cmdhelp import CmdHelp
+from AuraXUserbot.cmdhelp import CmdHelp
 
 
 @bot.on(admin_cmd(pattern="wikimedia (.*)"))
