aeaaf63bb64ee22695908dcdf6c32395c870d234
Gaurav Verma <gauravv2504@gmail.com>
2021-05-13 02:00:30 +0530

    Update animations4.py

--- AuraXUserbot/plugins/animations4.py
+++ AuraXUserbot/plugins/animations4.py
@@ -1,7 +1,7 @@
 import asyncio
 
 from AuraXBot.utils import admin_cmd, sudo_cmd, edit_or_reply
-from userbot.cmdhelp import CmdHelp
+from AuraXUserbot.cmdhelp import CmdHelp
 
 
 @bot.on(admin_cmd(pattern="phub$", outgoing=True))
