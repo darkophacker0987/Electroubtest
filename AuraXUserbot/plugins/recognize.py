0055ab63dfbf518f5d1aac4fafb40e6015365803
Gaurav Verma <gauravv2504@gmail.com>
2021-05-13 01:58:02 +0530

    Update recognize.py

--- AuraXUserbot/plugins/recognize.py
+++ AuraXUserbot/plugins/recognize.py
@@ -3,9 +3,9 @@
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 
-from userbot import CMD_HELP
+from AuraXUserbot import CMD_HELP
 from AuraXBot.utils import admin_cmd, sudo_cmd, edit_or_reply
-from userbot.cmdhelp import CmdHelp
+from AuraXUserbot.cmdhelp import CmdHelp
 
 
 @bot.on(admin_cmd(pattern="recognize ?(.*)", outgoing=True))
