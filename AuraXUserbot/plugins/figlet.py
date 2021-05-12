6a2b17b1e8a607d2b997bd67a6ef8e1c8149e629
Gaurav Verma <gauravv2504@gmail.com>
2021-05-13 01:54:08 +0530

    Update figlet.py

--- AuraXUserbot/plugins/figlet.py
+++ AuraXUserbot/plugins/figlet.py
@@ -1,8 +1,8 @@
 import pyfiglet
 
-from userbot import CMD_HELP
+from AuraXUserbot import CMD_HELP
 from AuraXBot.utils import admin_cmd, edit_or_reply, sudo_cmd
-from userbot.cmdhelp import CmdHelp
+from AuraXUserbot.cmdhelp import CmdHelp
 
 @bot.on(admin_cmd(pattern="figlet ?(.*)", outgoing=True))
 @bot.on(sudo_cmd(pattern="figlet ?(.*)", allow_sudo=True))
