4ac154eaa4375d6d3557b743626fa38fb2ca3fbc
Gaurav Verma <gauravv2504@gmail.com>
2021-05-13 01:59:57 +0530

    Update admin.py

--- AuraXUserbot/plugins/admin.py
+++ AuraXUserbot/plugins/admin.py
@@ -29,10 +29,10 @@ from telethon.tl.types import (
     MessageMediaPhoto,
 )
 
-from userbot import *
-from userbot.plugins.sql_helper.mute_sql import is_muted, mute, unmute
+from AuraXUserbot import *
+from AuraXUserbot.plugins.sql_helper.mute_sql import is_muted, mute, unmute
 from AuraXBot.utils import *
-from userbot.cmdhelp import CmdHelp
+from AuraXUserbot.cmdhelp import CmdHelp
 
 # =================== CONSTANT ===================
 
